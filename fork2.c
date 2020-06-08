70cb7bde57a95836e857e674415e9bdfacdfafd1
barcelona123456 <39547932+barcelona123456@users.noreply.github.com>
2020-06-08 21:41:36 +0900

    Add files via upload

---
+++ fork2.c
@@ -0,0 +1,13 @@
+#include <stdlib.h> 
+#include <stdio.h>
+ /* 부모 프로세스가 자식 프로세스를 생성하고 서로 다른 메시지를 프린트 */ 
+int main() 
+{  int pid; 
+   pid = fork(); 
+   if (pid ==0) {    
+   printf("[Child] : Hello, world pid = %d\n", getpid()); 
+   } 
+   else {    
+    printf("[Parent] : Hello, world pid = %d\n", getpid()); 
+   } 
+}
---
+++ forkwait.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+int main() 
+{ int pid, child, status; 
+  printf("[%d] 부모 프로세스 시작 \n", getpid( )); 
+  pid = fork(); 
+  if (pid == 0) { 
+  printf("[%d] 자식 프로세스 시작 \n", getpid( )); 
+  exit(1); 
+} 
+  child = wait(&status); // 자식 프로세스가 끝나기를 기다린다. 
+  printf("[%d] 자식 프로세스 %d 종료 \n", getpid(), child); 
+  printf("\t종료 코드 %d\n", status>>8); 
+} 
